1e6eebc24790c32f03f5f3b4a396739897bd6a23
AlanSDV61 <asosa0632@gmail.com>
2024-04-16 19:43:33 -0400

    + Changes

--- ProyectoUniversidad/Controllers/EstudianteController.cs
+++ ProyectoUniversidad/Controllers/EstudianteController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoUniversidad.Context;
 using ProyectoUniversidad.Models;
+using UniversidadAPI.Models;
 
 namespace ProyectoUniversidad.Controllers
 {
@@ -32,6 +33,7 @@ namespace ProyectoUniversidad.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Estudiante>> GetEstudiante(int id)
         {
+            
             var estudiante = await _context.Estudiante.FindAsync(id);
 
             if (estudiante == null)
@@ -43,10 +45,10 @@ namespace ProyectoUniversidad.Controllers
         }
 
         // PUT: api/Estudiante/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutEstudiante(int id, Estudiante estudiante)
         {
+            
             if (id != estudiante.estudiante_id)
             {
                 return BadRequest();
@@ -100,6 +102,73 @@ namespace ProyectoUniversidad.Controllers
             return NoContent();
         }
 
+        // GET: api/Estudiante/5/Selecciones
+        [HttpGet("{id}/Selecciones")]
+        public async Task<ActionResult<IEnumerable<Seleccion>>> GetEstudianteSelecciones(int id)
+        {
+
+
+            // Busca el estudiante por su ID
+            var estudiante = await _context.Estudiante.FindAsync(id);
+
+            // Si el estudiante no existe, devuelve un error 404
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+
+            // Busca las selecciones relacionadas con este estudiante
+            var selecciones = await _context.Seleccion
+                                    .Where(s => s.estudiante_id == id)
+                                    .ToListAsync();
+
+            return selecciones;
+        }
+
+        [HttpGet("{id}/Asignaturas")]
+        public async Task<ActionResult<IEnumerable<string>>> GetEstudianteAsignaturas(int id)
+        {
+            // Busca el estudiante por su ID
+            var estudiante = await _context.Estudiante.FindAsync(id);
+
+            // Si el estudiante no existe, devuelve un error 404
+            if (estudiante == null)
+            {
+                return NotFound();
+            }
+
+            // Busca las selecciones del estudiante
+            var selecciones = await _context.Seleccion
+                                    .Where(s => s.estudiante_id == id)
+                                    .ToListAsync();
+
+            // Lista para almacenar los nombres de las asignaturas
+            var nombresAsignaturas = new List<string>();
+
+            // Itera sobre cada selección del estudiante
+            foreach (var seleccion in selecciones)
+            {
+                // Busca las entradas en la tabla puente asignatura_seleccion para esta selección
+                var asignaturasSeleccion = await _context.Asignatura_seleccion
+                                                .Where(asignSel => asignSel.seleccion_id == seleccion.seleccion_id)
+                                                .ToListAsync();
+
+                // Para cada entrada en asignatura_seleccion, busca el nombre de la asignatura
+                foreach (var asignaturaSeleccion in asignaturasSeleccion)
+                {
+                    // Busca el nombre de la asignatura por su ID
+                    var asignatura = await _context.Asignatura.FindAsync(asignaturaSeleccion.asignatura_id);
+
+                    // Si la asignatura existe, agrega su nombre a la lista de nombres de asignaturas
+                    if (asignatura != null)
+                    {
+                        nombresAsignaturas.Add(asignatura.asignatura_nombre);
+                    }
+                }
+            }
+
+            return nombresAsignaturas;
+        }
         private bool EstudianteExists(int id)
         {
             return _context.Estudiante.Any(e => e.estudiante_id == id);
