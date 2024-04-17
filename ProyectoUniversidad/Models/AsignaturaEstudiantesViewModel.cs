7fac11e3631a6eb5026ab50a9f2ae3025b9ec186
AlanSDV61 <asosa0632@gmail.com>
2024-04-17 00:08:07 -0400

    Changes AsignaturaEstudiantesViewModel

--- ProyectoUniversidad/Models/AsignaturaEstudiantesViewModel.cs
+++ ProyectoUniversidad/Models/AsignaturaEstudiantesViewModel.cs
@@ -4,10 +4,12 @@
     {
         public int asignatura_id { get; set; }
         public string? asignatura_nombre { get; set; }
+        public string? asignatura_aula { get; set; }
 
+        public int asignatura_creditos { get; set; }
         public int profesor_id { get; set; }
-
         public string? profesor_nombre { get; set; }
+
         public List<Estudiante>? Estudiantes { get; set; }
     }
 }
