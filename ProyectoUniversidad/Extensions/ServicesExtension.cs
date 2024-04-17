9f59c34119ee78ba5e5eb6f673eb8ed0997daea3
AlanSDV61 <asosa0632@gmail.com>
2024-04-17 00:27:19 -0400

    fixs CORS

---
+++ ProyectoUniversidad/Extensions/ServicesExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoUniversidad.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurarCORS(this IServiceCollection services, string MyAllowSpecifiOrigins)
+        {
+            string[] domains = { "https://productionweb-production.up.railway.app", "http://localhost:8100" };
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecifiOrigins,
+                    policy =>
+                    {
+                        policy.WithOrigins(domains)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+        }
+
+    }
+}
