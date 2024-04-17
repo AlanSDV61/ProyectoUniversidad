3c44739a8e7cef4f2291f05672b24aa9c53641b8
AlanSDV61 <asosa0632@gmail.com>
2024-04-17 00:24:04 -0400

    12:23 am

--- ProyectoUniversidad/Models/AsignaturaEstudiantesViewModel.cs
+++ ProyectoUniversidad/Models/AsignaturaEstudiantesViewModel.cs
@@ -5,11 +5,9 @@
         public int asignatura_id { get; set; }
         public string? asignatura_nombre { get; set; }
         public string? asignatura_aula { get; set; }
-
         public int asignatura_creditos { get; set; }
         public int profesor_id { get; set; }
         public string? profesor_nombre { get; set; }
-
         public List<Estudiante>? Estudiantes { get; set; }
     }
 }
